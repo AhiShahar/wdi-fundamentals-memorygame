7b484cf8633e9f2c0f57ddf985836fe3e67e33a6
Ahi Shahar <ahishahar@Ahis-MacBook-Air.local>
2016-10-23 13:01:22 +1100

    adding cards functions

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,8 +4,22 @@ var cardTwo = 'queen';
 var cardThree = 'king';
 var cardFour = 'king';
 
+/*
 if (cardTwo === cardFour) {
 	alert("You have found a match!");
 } else {
 	alert("Sorry, try again!")
 }
+*/
+
+var gameBoard = document.getElementById("#game-board");
+
+var createCards = function() {
+	for (i = 0; i < 4; i++) {
+		var card = document.createElement('div');
+		card.className = 'card';
+		gameBoard.appendChild(card);
+	}
+}
+
+createCards();
